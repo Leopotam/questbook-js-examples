bb893ad856657e2ff82bd19f16557569b6f9356f
Leopotam <leopotam@gmail.com>
2017-07-26 23:25:21 +0300

    * Update to questbook@1.0.1.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { QuestLoader, QuestDocument } from 'questbook';
 import ConsoleRunner from './console-runner';
 
 const markup = fs.readFileSync('./quest.txt', 'utf8');
-const parsedDoc = QuestLoader.loadMarkup(markup, true);
+const parsedDoc = QuestLoader.loadMarkup(markup);
+QuestLoader.validate(parsedDoc);
 const runner = new ConsoleRunner(parsedDoc);
